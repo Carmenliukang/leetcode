849bd0aba1b16fb4d31c8ce581442354b5957b82
刘康 <liukang.hero@ad.bytedance.com>
2020-05-21 17:30:13 +0800

    2020-05-21 算计基础更新

---
+++ 算法/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: liukang
+@file: __init__.py.py
+@time: 2020-05-21 17:26
+@desc:
+'''
---
+++ 算法/合并两个有序链表.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: liukang
+@file: 合并两个有序链表.py
+@time: 2020-05-21 17:28
+@desc:
+'''
+
+'''
+使用递归的方式完成数据同步
+'''
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1: return l2  # 终止条件，直到两个链表都空
+        if not l2: return l1
+        if l1.val <= l2.val:  # 递归调用
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
