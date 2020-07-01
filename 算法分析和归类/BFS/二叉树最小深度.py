fb99fa3be802978a800c81cf169c7332476eed87
刘康 <17621173188@163.com>
2020-07-01 14:46:40 +0800

    add BFS

--- 算法分析和归类/BFS/二叉树最小深度.py
+++ 算法分析和归类/BFS/二叉树最小深度.py
@@ -3,5 +3,39 @@
 
 from collections import deque
 
-queue = deque("1")
-print(len(queue))
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+
+        if not root:
+            return 0
+
+        step = 1
+        # 初始化队列，但是有一些小问题。
+        queue = deque()
+        queue.append(root)
+
+        while queue:
+            for i in range(len(queue)):
+
+                cur = queue.popleft()
+                if cur.left == None and cur.right == None:
+                    return step
+
+                if cur.left != None:
+                    queue.append(cur.left)
+
+                if cur.right != None:
+                    queue.append(cur.right)
+
+            step += 1
+
+        return step
