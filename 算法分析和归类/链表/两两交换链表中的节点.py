0ab4562c97fbb117af943c28919d99c23c1cfddf
Carmenliukang <17621173188@163.com>
2020-10-10 20:23:37 +0800

    1. 查看了官方的解法，真的是简洁了，太厉害了。

--- 算法分析和归类/链表/两两交换链表中的节点.py
+++ 算法分析和归类/链表/两两交换链表中的节点.py
@@ -54,6 +54,36 @@ class Solution:
         # Now the head is the second node
         return second_node
 
+    def swapPairsMethod1(self, head: ListNode) -> ListNode:
+        """
+        这个是一个官方推荐的方法，确实代码非常简洁，比自己的方法写的太好了。
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        # Dummy node acts as the prevNode for the head node
+        # of the list and hence stores pointer to the head node.
+        dummy = ListNode(-1)
+        dummy.next = head
+
+        prev_node = dummy
+
+        while head and head.next:
+            # Nodes to be swapped
+            first_node = head;
+            second_node = head.next;
+
+            # Swapping
+            prev_node.next = second_node
+            first_node.next = second_node.next
+            second_node.next = first_node
+
+            # Reinitializing the head and prev_node for next swap
+            prev_node = first_node
+            head = first_node.next
+
+        # Return the new head node.
+        return dummy.next
+
     def swapPairsMethod(self, head: ListNode) -> ListNode:
         """
         1. 特殊情况处理
