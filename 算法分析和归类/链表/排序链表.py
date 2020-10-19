3712bbf22ab26a19f7fa1798413302b7a523e094
刘康 <liukang.hero@bytedance.com>
2020-10-19 11:46:34 +0800

    1. 排序链表

--- 算法分析和归类/链表/排序链表.py
+++ 算法分析和归类/链表/排序链表.py
@@ -57,3 +57,89 @@ class Solution:
             root = root.next
 
         return result.next
+
+    def sortListMethod(self, head: ListNode) -> ListNode:
+        # TODO 这里还需要再次看一下
+        """
+        解答一：归并排序（递归法）
+        题目要求时间空间复杂度分别为O(nlogn)O(nlogn)和O(1)O(1)，根据时间复杂度我们自然想到二分法，从而联想到归并排序；
+
+        对数组做归并排序的空间复杂度为 O(n)O(n)，分别由新开辟数组O(n)O(n)和递归函数调用O(logn)O(logn)组成，而根据链表特性：
+
+        数组额外空间：链表可以通过修改引用来更改节点顺序，无需像数组一样开辟额外空间；
+        递归额外空间：递归调用函数将带来O(logn)O(logn)的空间复杂度，因此若希望达到O(1)O(1)空间复杂度，则不能使用递归。
+        通过递归实现链表归并排序，有以下两个环节：
+
+        分割 cut 环节： 找到当前链表中点，并从中点将链表断开（以便在下次递归 cut 时，链表片段拥有正确边界）；
+        我们使用 fast,slow 快慢双指针法，奇数个节点找到中点，偶数个节点找到中心左边的节点。
+        找到中点 slow 后，执行 slow.next = None 将链表切断。
+        递归分割时，输入当前链表左端点 head 和中心节点 slow 的下一个节点 tmp(因为链表是从 slow 切断的)。
+        cut 递归终止条件： 当head.next == None时，说明只有一个节点了，直接返回此节点。
+        合并 merge 环节： 将两个排序链表合并，转化为一个排序链表。
+        双指针法合并，建立辅助ListNode h 作为头部。
+        设置两指针 left, right 分别指向两链表头部，比较两指针处节点值大小，由小到大加入合并链表头部，指针交替前进，直至添加完两个链表。
+        返回辅助ListNode h 作为头部的下个节点 h.next。
+        时间复杂度 O(l + r)，l, r 分别代表两个链表长度。
+        当题目输入的 head == None 时，直接返回None。
+
+        作者：jyd
+        链接：https://leetcode-cn.com/problems/sort-list/solution/sort-list-gui-bing-pai-xu-lian-biao-by-jyd/
+        来源：力扣（LeetCode）
+        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+        :param head:
+        :return:
+        """
+        # 递归方式解决问题
+        # https://pic.leetcode-cn.com/8c47e58b6247676f3ef14e617a4686bc258cc573e36fcf67c1b0712fa7ed1699-Picture2.png
+        if not head or not head.next: return head  # termination.
+        # cut the LinkedList at the mid index.
+        slow, fast = head, head.next
+        while fast and fast.next:
+            fast, slow = fast.next.next, slow.next
+        mid, slow.next = slow.next, None  # save and cut.
+        # recursive for cutting.
+        left, right = self.sortList(head), self.sortList(mid)
+        # merge `left` and `right` linked list and return it.
+        h = res = ListNode(0)
+        while left and right:
+            if left.val < right.val:
+                h.next, left = left, left.next
+            else:
+                h.next, right = right, right.next
+            h = h.next
+        h.next = left if left else right
+        return res.next
+
+    def sortListMethod2(self, head: ListNode) -> ListNode:
+        # TODO 这里还需要再次看一下
+        # https://leetcode-cn.com/problems/sort-list/solution/sort-list-gui-bing-pai-xu-lian-biao-by-jyd/
+        # 归并排序 方式，实现字底向上的同步
+        #
+        h, length, intv = head, 0, 1
+        while h: h, length = h.next, length + 1
+        res = ListNode(0)
+        res.next = head
+        # merge the list in different intv.
+        while intv < length:
+            pre, h = res, res.next
+            while h:
+                # get the two merge head `h1`, `h2`
+                h1, i = h, intv
+                while i and h: h, i = h.next, i - 1
+                if i: break  # no need to merge because the `h2` is None.
+                h2, i = h, intv
+                while i and h: h, i = h.next, i - 1
+                c1, c2 = intv, intv - i  # the `c2`: length of `h2` can be small than the `intv`.
+                # merge the `h1` and `h2`.
+                while c1 and c2:
+                    if h1.val < h2.val:
+                        pre.next, h1, c1 = h1, h1.next, c1 - 1
+                    else:
+                        pre.next, h2, c2 = h2, h2.next, c2 - 1
+                    pre = pre.next
+                pre.next = h1 if c1 else h2
+                while c1 > 0 or c2 > 0: pre, c1, c2 = pre.next, c1 - 1, c2 - 1
+                pre.next = h
+            intv *= 2
+        return res.next
