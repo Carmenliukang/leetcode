5f310fd1ad9bd08c06b73bfe95c3d70b848a4ab1
刘康 <liukang.hero@bytedance.com>
2020-10-22 14:25:55 +0800

    1. 分割链表，使用了双指针的方式同步。

---
+++ 算法分析和归类/链表/分割链表.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+编写程序以 x 为基准分割链表，使得所有小于 x 的节点排在大于或等于 x 的节点之前。如果链表中包含 x，x 只需出现在小于 x 的元素之后(如下所示)。分割元素 x 只需处于“右半部分”即可，其不需要被置于左右两部分之间。
+
+示例:
+
+输入: head = 3->5->8->5->10->2->1, x = 5
+输出: 3->1->2->10->5->5->8
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/partition-list-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def partition(self, head: ListNode, x: int) -> ListNode:
+        # 先获取结果，同步最初的目标
+
+        if not head:
+            return head
+
+        # 依次进行指针的同步
+        left = ListNode(-1)
+        right = ListNode(-1)
+        right_start, left_start = right, left
+
+        while head:
+            # 对于下一个尝试同步
+            next = head.next
+
+            if head.val < x:
+                left.next = head
+                left = left.next
+            else:
+                right.next = head
+                right = right.next
+
+            # 这里为什么要断开呢？
+            # TODO 这里需要思考一下
+            head.next = None
+
+            head = next
+
+        left.next = right_start.next
+
+        return left_start.next
