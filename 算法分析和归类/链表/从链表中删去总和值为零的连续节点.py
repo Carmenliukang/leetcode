bb33771e2f67437a24fa47b091056ae752b63bdf
刘康 <liukang.hero@bytedance.com>
2020-10-30 21:31:56 +0800

    1. 完成链表删除连续节点 为0的节点。

---
+++ 算法分析和归类/链表/从链表中删去总和值为零的连续节点.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+给你一个链表的头节点 head，请你编写代码，反复删去链表中由 总和 值为 0 的连续节点组成的序列，直到不存在这样的序列为止。
+
+删除完毕后，请你返回最终结果链表的头节点。
+
+ 
+
+你可以返回任何满足题目要求的答案。
+
+（注意，下面示例中的所有序列，都是对 ListNode 对象序列化的表示。）
+
+示例 1：
+
+输入：head = [1,2,-3,3,1]
+输出：[3,1]
+提示：答案 [1,2,1] 也是正确的。
+示例 2：
+
+输入：head = [1,2,3,-3,4]
+输出：[1,2,4]
+示例 3：
+
+输入：head = [1,2,3,-3,-2]
+输出：[1]
+
+提示：
+
+给你的链表中可能有 1 到 1000 个节点。
+对于链表中的每个节点，节点的值：-1000 <= node.val <= 1000.
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def removeZeroSumSublists(self, head: ListNode) -> ListNode:
+        total = 0
+
+        dummy = ListNode(0)
+        dummy.next = head
+
+        # 首次遍历建立 节点处链表和<->节点 哈希表
+        # 若同一和出现多次会覆盖，即记录该sum出现的最后一次节点
+        root = dummy
+        sum_dict = {}
+        while root:
+            total += root.val
+            sum_dict[total] = root
+            root = root.next
+
+        # 第二遍遍历 若当前节点处sum在下一处出现了则表明两结点之间所有节点和为0 直接删除区间所有节点
+        total = 0
+        root = dummy
+        while root:
+            total += root.val
+            root.next = sum_dict[total].next
+            root = root.next
+
+        return dummy.next
