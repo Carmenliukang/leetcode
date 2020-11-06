46c78bc4253c648558a779d9379f778bdd49f815
刘康 <liukang.hero@bytedance.com>
2020-11-06 11:19:15 +0800

    1. 递增顺序查找数方式同步。

---
+++ 算法分析和归类/树/递增顺序查找树.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+给你一个树，请你 按中序遍历 重新排列树，使树中最左边的结点现在是树的根，并且每个结点没有左子结点，只有一个右子结点。
+
+ 
+
+示例 ：
+
+输入：[5,3,6,2,4,null,8,1,null,null,null,7,9]
+
+       5
+      / \
+    3    6
+   / \    \
+  2   4    8
+ /        / \
+1        7   9
+
+输出：[1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+
+ 1
+  \
+   2
+    \
+     3
+      \
+       4
+        \
+         5
+          \
+           6
+            \
+             7
+              \
+               8
+                \
+                 9
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/increasing-order-search-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def increasingBST(self, root):
+        # 这里使用的是 yield 方式同步，能够节省内存，这里真的太赞了
+        def inorder(node):
+            if node:
+                yield from inorder(node.left)
+                yield node.val
+                yield from inorder(node.right)
+
+        # 测试同步账户
+        ans = cur = TreeNode(None)
+        for v in inorder(root):
+            cur.right = TreeNode(v)
+            cur = cur.right
+        return ans.right
