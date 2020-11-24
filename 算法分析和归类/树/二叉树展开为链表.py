33cbd42a11ad895414edc1ad04dfe93c1b963136
刘康 <liukang.hero@bytedance.com>
2020-11-25 00:54:46 +0800

    1. 链表展开 2. week 16 down

--- 算法分析和归类/树/二叉树展开为链表.py
+++ 算法分析和归类/树/二叉树展开为链表.py
@@ -36,11 +36,13 @@
 
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
 
     def flatten(self, root: TreeNode) -> None:
