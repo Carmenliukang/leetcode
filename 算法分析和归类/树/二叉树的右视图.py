621ddaf9a89fc18967d89bf455f7bffb39a7a057
刘康 <liukang.hero@bytedance.com>
2020-11-25 19:39:32 +0800

    1. 二叉树的右视图，这个算法确实非常巧妙。

---
+++ 算法分析和归类/树/二叉树的右视图.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+
+示例:
+
+输入: [1,2,3,null,5,null,4]
+输出: [1, 3, 4]
+解释:
+
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-right-side-view
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def rightSideView(self, root):
+        rightmost_value_at_depth = dict()  # 深度为索引，存放节点的值
+        max_depth = -1
+
+        stack = [(root, 0)]
+        while stack:
+            # pop 是从最右边进行删除的
+            node, depth = stack.pop()
+
+            if node is not None:
+                # 维护二叉树的最大深度
+                max_depth = max(max_depth, depth)
+
+                # 如果不存在对应深度的节点我们才插入
+                rightmost_value_at_depth.setdefault(depth, node.val)
+                # 先加入到左节点，然后再加入右节点，这样能够保证 右边的数值一定是在最右边
+                stack.append((node.left, depth + 1))
+                stack.append((node.right, depth + 1))
+
+        return [rightmost_value_at_depth[depth] for depth in range(max_depth + 1)]
