2e3cf4871064f6fadef9e1aa0a5f5ec4f5d0c35e
刘康 <liukang.hero@bytedance.com>
2020-12-06 14:36:24 +0800

    1. 不同的二叉搜索树 || 完成。这个可以类比满二叉树类型。

---
+++ 算法分析和归类/树/不同的二叉搜索树 II.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+给定一个整数 n，生成所有由 1 ... n 为节点所组成的 二叉搜索树 。
+
+ 
+
+示例：
+
+输入：3
+输出：
+[
+  [1,null,3,2],
+  [3,2,null,1],
+  [3,1,null,null,2],
+  [2,1,3],
+  [1,null,2,null,3]
+]
+解释：
+以上的输出对应以下 5 种不同结构的二叉搜索树：
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+ 
+
+提示：
+
+0 <= n <= 8
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/unique-binary-search-trees-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def generateTrees(self, n: int) -> list[TreeNode]:
+        if not n:
+            return []
+        return self.dfs(1, n)
+
+    def dfs(self, start, end):
+        # https://leetcode-cn.com/problems/unique-binary-search-trees-ii/solution/cong-gou-jian-dan-ke-shu-dao-gou-jian-suo-you-shu-/
+        if start > end:
+            return [None, ]
+
+        res = []
+        for i in range(start, end + 1):
+            left = self.dfs(start, i - 1)
+            right = self.dfs(i + 1, end)
+            for l in left:
+                for r in right:
+                    root = TreeNode(i)
+                    root.left = l
+                    root.right = r
+                    res.append(root)
+
+        return res
