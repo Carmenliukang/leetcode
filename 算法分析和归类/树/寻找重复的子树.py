292ece6de2f30add14e10a5f03178acc7f5b5a73
刘康 <17621173188@163.com>
2020-12-14 21:56:07 +0800

    1. 返回其具体的数据。

---
+++ 算法分析和归类/树/寻找重复的子树.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+给定一棵二叉树，返回所有重复的子树。对于同一类的重复子树，你只需要返回其中任意一棵的根结点即可。
+
+两棵树重复是指它们具有相同的结构以及相同的结点值。
+
+示例 1：
+
+        1
+       / \
+      2   3
+     /   / \
+    4   2   4
+       /
+      4
+下面是两个重复的子树：
+
+      2
+     /
+    4
+和
+
+    4
+因此，你需要以列表的形式返回上述重复子树的根结点。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-duplicate-subtrees
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+"""
+
+from collections import defaultdict
+from collections import Counter
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def __init__(self):
+        self.res = []
+
+    def findDuplicateSubtrees(self, root: TreeNode) -> list[TreeNode]:
+        # 数据同步修改。
+        trees = defaultdict()
+        # 这里是返回去具体的长度
+        trees.default_factory = trees.__len__
+        # 这个是一个计数器，自主增加
+        count = Counter()
+        ans = []
+
+        def lookup(node):
+            if node:
+                uid = trees[node.val, lookup(node.left), lookup(node.right)]
+                count[uid] += 1
+                if count[uid] == 2:
+                    ans.append(node)
+                return uid
+
+        lookup(root)
+        return ans
