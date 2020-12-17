1045bcb580c7415728b4845b293ceba4258c3220
刘康 <17621173188@163.com>
2020-12-17 13:36:51 +0800

    1. 完成其相关的数据的同步。

---
+++ 算法分析和归类/树/路径总和 III.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+给定一个二叉树，它的每个结点都存放着一个整数值。
+
+找出路径和等于给定数值的路径总数。
+
+路径不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+
+二叉树不超过1000个节点，且节点数值范围是 [-1000000,1000000] 的整数。
+
+示例：
+
+root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+      10
+     /  \
+    5   -3
+   / \    \
+  3   2   11
+ / \   \
+3  -2   1
+
+返回 3。和等于 8 的路径有:
+
+1.  5 -> 3
+2.  5 -> 2 -> 1
+3.  -3 -> 11
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/path-sum-iii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def __init__(self):
+        self.res = 0
+
+    def pathSum(self, root: TreeNode, sum: int) -> int:
+        # 使用两次 dfs 进行计算
+        if not root:
+            return 0
+        return self.dfs(root, sum) + self.pathSum(root.left, sum) + self.pathSum(root.right, sum)
+
+    def dfs(self, root, sum):
+        if not root:
+            return 0
+        tmp = 0
+        sum = sum - root.val
+        if sum == 0:
+            tmp += 1
+
+        return tmp + self.dfs(root.left, sum) + self.dfs(root.right, sum)
