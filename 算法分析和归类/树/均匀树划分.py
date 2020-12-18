3208deb483eeafeeb2c4d44bdc374369a1f6f881
liukang <p2U5jLq9nkWMCTi>
2020-12-18 22:16:49 +0800

    1. 算法部分完成。

--- 算法分析和归类/树/均匀树划分.py
+++ 算法分析和归类/树/均匀树划分.py
@@ -53,11 +53,11 @@
 
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
     def __init__(self):
