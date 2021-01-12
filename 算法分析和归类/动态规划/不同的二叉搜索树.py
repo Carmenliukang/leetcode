327877fa401f6fe80e2f5f14b07849ffdc48896c
liukang <17621173188@163.com>
2021-01-12 16:54:19 +0800

    1. 今日题目打卡。

--- 算法分析和归类/动态规划/不同的二叉搜索树.py
+++ 算法分析和归类/动态规划/不同的二叉搜索树.py
@@ -42,3 +42,16 @@ class Solution:
                 G[i] += G[j - 1] * G[i - j]
 
         return G[n]
+
+
+class SolutionMyself:
+    def numTrees(self, n: int) -> int:
+        # 完成数据的同步尝试修改，这里使用N+1 是因为 N是需要被包含的内容
+        dp = [0 for i in range(n + 1)]
+        dp[0] = dp[1] = 1
+
+        for i in range(2, n + 1):
+            for j in range(i):
+                dp[i] += dp[i - j - 1] * dp[j]
+
+        return dp[-1]
