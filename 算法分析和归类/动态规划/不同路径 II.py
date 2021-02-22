862ac68cc400422d6e18da5e4708069cf35f7462
liukang <17621173188@163.com>
2021-02-23 01:04:09 +0800

    不同路径 II 完成

---
+++ 算法分析和归类/动态规划/不同路径 II.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+
+现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+
+
+
+网格中的障碍物和空位置分别用 1 和 0 来表示。
+
+
+
+示例 1：
+
+
+输入：obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+输出：2
+解释：
+3x3 网格的正中间有一个障碍物。
+从左上角到右下角一共有 2 条不同的路径：
+1. 向右 -> 向右 -> 向下 -> 向下
+2. 向下 -> 向下 -> 向右 -> 向右
+示例 2：
+
+
+输入：obstacleGrid = [[0,1],[0,0]]
+输出：1
+
+
+提示：
+
+m ==obstacleGrid.length
+n ==obstacleGrid[i].length
+1 <= m, n <= 100
+obstacleGrid[i][j] 为 0 或 1
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/unique-paths-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+"""
+
+
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [[0] * n for _ in range(m)]
+        if obstacleGrid[0][0] != 1:
+            dp[0][0] = 1
+
+        for _ in range(1, m):
+            if dp[_ - 1][0] == 0 or obstacleGrid[_][0] == 1:
+                dp[_][0] = 0
+            else:
+                dp[_][0] = 1
+
+        for _ in range(1, n):
+            if dp[0][_ - 1] == 0 or obstacleGrid[0][_] == 1:
+                dp[0][_] = 0
+            else:
+                dp[0][_] = 1
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if obstacleGrid[i][j] != 1:
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[-1][-1]
