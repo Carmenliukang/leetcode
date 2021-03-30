65b26ba89ddee147397f512b370f075d6e46a7c2
liukang <17621173188@163.com>
2021-03-30 19:03:50 +0800

    完成数据同步修复

--- 算法分析和归类/动态规划/零钱兑换 II.py
+++ 算法分析和归类/动态规划/零钱兑换 II.py
@@ -41,3 +41,17 @@
 
 """
 
+
+## todo 经典讲解 https://leetcode-cn.com/problems/coin-change-2/solution/ling-qian-dui-huan-iihe-pa-lou-ti-wen-ti-dao-di-yo/
+
+
+class Solution:
+    def change(self, amount: int, coins: list[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] += dp[i - coin]
+
+        return dp[amount]
