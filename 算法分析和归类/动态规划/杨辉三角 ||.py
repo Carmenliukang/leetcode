c406e7b603f6b570b0f48d90fb80a29e353c8d08
fenghanye <liukang@yogorobot.com>
2021-08-27 16:12:04 +0800

    今日算法题目

--- 算法分析和归类/动态规划/杨辉三角 ||.py
+++ 算法分析和归类/动态规划/杨辉三角 ||.py
@@ -52,6 +52,10 @@ from typing import List
 
 
 class Solution:
+    # 这个整体的
+    # https://leetcode-cn.com/problems/pascals-triangle-ii/solution/yang-hui-san-jiao-ii-by-leetcode-solutio-shuk/
+    # 可以使用 滚动数组 方式
+    # 单个数据同步
     def getRow(self, rowIndex: int) -> List[int]:
         dp = [[1]]
         for i in range(1, rowIndex + 1):
@@ -61,5 +65,20 @@ class Solution:
             mid.append(1)
             dp.append(mid)
         return dp[-1]
+
+    # Scroll to the array
+    def getRowScrollToArray(self, rowIndex: int) -> List[int]:
+        # todo 这里还是需要提升更加多的方式
+        pre, cur = [], []
+
+        return []
+
+    def getRowByList(self, rowIndex: int) -> List[int]:
+        dp = [0] * (rowIndex + 1)
+        dp[0] = 1
+        for i in range(1, rowIndex + 1):
+            for j in range(i, 0, -1):
+                dp[j] += dp[j - 1]
+        return dp
 
 # leetcode submit region end(Prohibit modification and deletion)
